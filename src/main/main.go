1e1338721f8f0418d683a4f7679852924deec8f8
ruteru64 <ruteru64@gmail.com>
2022-06-17 14:18:11 +0900

    入力を構造体の配列に

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"io"
+	"time"
 	"encoding/csv"
 	"log"
 	"os"
 	"fmt"
+	"strconv"
 )
+
+const timeFormat = "2021/01/01 12:00"
+
+type GameScore struct{
+	create_timestamp time.Time
+	player_id string
+	score int64
+}
 
 func main(){
 	fmt.Println(os.Args[1])
@@ -13,7 +24,7 @@ func main(){
 	fmt.Println(CSV)
 }
 
-func getCsv(filename string)[][]string{
+func getCsv(filename string)[]GameScore{
 	file,err := os.Open(filename)
 	if err != nil{
 		log.Fatal(err)
@@ -21,11 +32,33 @@ func getCsv(filename string)[][]string{
 	defer file.Close()
 
 	r := csv.NewReader(file)
-	rows,err := r.ReadAll()
+	var gameScores []GameScore
 
-	if err != nil{
-		log.Fatal(err)
+	// 1行目を飛ばす
+	r.Read()
+
+
+	for{
+		row,err := r.Read()
+		if err != io.EOF{
+			break
+		}
+		var t GameScore
+		t.create_timestamp = stoTime(row[0])
+		t.player_id = row[1]
+		temp,_ := strconv.Atoi(row[2])
+		t.score = int64(temp)
+		gameScores = append(gameScores,t)
 	}
+	return gameScores
+}
 
-	return rows
+func stoTime(s string)time.Time{
+	t,_ := time.Parse(timeFormat,s)
+	return t
+}
+
+func timetos(t time.Time)string{
+	s := t.Format(timeFormat)
+	return s
 }
